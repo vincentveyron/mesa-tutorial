a5f98906093d63d7ec3f9dd08072a66ed3b60770
Vincent Gray <vincentveyron@aol.com>
2020-01-23 19:52:37 +0000

    add schedule and step methods

--- MoneyModel.py
+++ MoneyModel.py
@@ -1,4 +1,5 @@
 from mesa import Agent, Model
+from mesa.time import RandomActivation
 
 
 class MoneyAgent(Agent):
@@ -7,11 +8,21 @@ class MoneyAgent(Agent):
         super().__init__(unique_ID, model)
         self.wealth = 1
 
+    def step(self):
+        """the agent step will go here"""
+        pass
+
 
 class MoneyModel(Model):
     """a model with some number of agents"""
     def __init__(self, N):
         self.num_agents = N
+        self.schedule = RandomActivation(self)
         # create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        """advance the model by one step"""
+        self.schedule.step()

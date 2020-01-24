29a5927d199d7890a6824d30d78bbcb07563bda5
Vincent Gray <vincentveyron@aol.com>
2020-01-24 18:47:43 +0000

    add move method to agent

--- MoneyModel.py
+++ MoneyModel.py
@@ -18,6 +18,15 @@ class MoneyAgent(Agent):
         other_agent.wealth += 1
         self.wealth -= 1
 
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
 
 class MoneyModel(Model):
     """a model with some number of agents"""

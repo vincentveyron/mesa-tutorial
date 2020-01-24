198f6bd2e8f683214d6cc6d027fa4c7dacd07138
Vincent Gray <vincentveyron@aol.com>
2020-01-24 18:56:43 +0000

    finish basic spacial element

--- MoneyModel.py
+++ MoneyModel.py
@@ -11,12 +11,9 @@ class MoneyAgent(Agent):
 
     def step(self):
         """the agent step will go here"""
-        if self.wealth == 0:
-            return
-
-        other_agent = self.random.choice(self.model.schedule.agents)
-        other_agent.wealth += 1
-        self.wealth -= 1
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
@@ -26,6 +23,13 @@ class MoneyAgent(Agent):
         )
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other = self.random.choice(cellmates)
+            other.wealth += 1
+            self.wealth -= 1
 
 
 class MoneyModel(Model):
